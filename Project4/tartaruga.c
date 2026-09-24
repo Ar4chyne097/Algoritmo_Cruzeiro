af70dd26a15b31b3f555e3e9b0d998ce93f0122e
Alice <alice86975@gmail.com>
2026-09-24 16:11:42 -0300

    :3

--- Project4/tartaruga.c
+++ Project4/tartaruga.c
@@ -35,7 +35,7 @@ int main(int argc, char** argv) {
 	}*/
 	
 	//Questão 5
-	int a, i;
+	/*int a, i;
 	
 	printf("Digite um número:");
 	scanf("%d", &a);
@@ -44,5 +44,77 @@ int main(int argc, char** argv) {
 		a = a + i;
 	}
 	
-	printf("%d", a);
+	printf("%d", a);*/
+	
+	//Questão 6
+	/*int a[10], i1, i2;
+	
+	for(i1 = 0; i1 <= 9; i1++){
+		printf("Digite o %dº numero:", i1+1);
+		scanf("%d", &a[i1]);
+	}
+	
+	int soma = 0;
+	for(i2 = 0; i2<= 9; i2++){
+		soma = soma + a[i2];
+	}
+	printf("A soma de todos os 10 números fornecidos é: %d", soma);*/
+	
+	//Questão 7
+	/*int a[10], i, contagem = 0;
+	
+	for(i = 0; i <= 9; i++){
+		printf("Digite o %d° numero:", i+1);
+		scanf("%d", &a[i]);
+	}
+	
+	for(i = 0; i <= 9; i++){
+		if(a[i] < 0){
+			contagem = contagem + 1;
+		}	
+	}
+	
+	printf("A quantidade de valores negativos fornecidos foram: %d", contagem);*/
+	
+	//Questão 8
+	/*int a[50], i, soma;
+	soma = 0;
+	
+	printf("Digite o primeiro valor:");
+	scanf("%d", &a[0]);
+	
+	if(a[0] == 0){
+		printf("%d", soma);
+	}else{
+		for(i = 1; i < 50; i++){
+			printf("Digite o próximo valor:");
+			scanf("%d", &a[i]);
+			
+			if(a[i] == 0){
+				break;
+			}
+			
+			soma = soma + a[i];
+		}
+		soma = soma + a[0];
+		printf("%d", soma);
+	}*/
+	
+	//Questão 9
+	/*int i;
+	for(i = 1; i <= 33; i++){
+		printf("%d\n",(3*i));
+	}*/
+	
+	//Questão 10
+	/*int i, a;
+	
+	printf("Digite o número limite:");
+	scanf("%d", &a);
+	
+	for(i = 0; i <= a; i++){
+		if(i % 2 != 0){
+			printf("%d\n", i);
+		}
+	}*/
 }
